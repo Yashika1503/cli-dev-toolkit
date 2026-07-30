1a2c23cded9be9a3c7a8a87d4928890631804e5e
Yashika Gaur <gyashika1503@gmail.com>
2026-07-30 17:31:12 +0530

    feat(gen-env): read project files

--- services/envGenerator.js
+++ services/envGenerator.js
@@ -1,3 +1,16 @@
+import { glob } from "glob";
+import { readFile } from "node:fs/promises";
+
 export default async function generateEnv() {
-    console.log("Generating .env.example...");
+    const files = await glob("**/*.js", {
+        ignore: [
+            "node_modules/**"
+        ]
+    });
+
+    for (const file of files) {
+        const content = await readFile(file, "utf8");
+
+        console.log(`${file} (${content.length} characters)`);
+    }
 }
