f8af4e3cf55ccaf1ea634dbaee6863e26e0576a6
Yashika Gaur <gyashika1503@gmail.com>
2026-07-30 18:24:06 +0530

    feat(gen-env): collect unique environment variables

--- services/envGenerator.js
+++ services/envGenerator.js
@@ -1,6 +1,8 @@
 import { glob } from "glob";
 import { readFile } from "node:fs/promises";
 
+const port = process.env.PORT;
+
 export default async function generateEnv() {
     const files = await glob("**/*.js", {
         ignore: [
@@ -8,9 +10,21 @@ export default async function generateEnv() {
         ]
     });
 
+    const envVars = new Set();
+
     for (const file of files) {
         const content = await readFile(file, "utf8");
 
-        console.log(`${file} (${content.length} characters)`);
+        const regex = /process\.env\.([A-Za-z0-9_]+)/g;
+
+        const matches = content.matchAll(regex);
+
+        for (const match of matches) {
+            envVars.add(match[1]);
+        }
     }
+
+    const variables = [...envVars].sort();
+
+    console.log(variables);
 }
